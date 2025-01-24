f90d2f2f36e3746a378dcab50ff76115d11fc5e1
☁️淡然 <hi@ydr.me>
2025-01-24 14:25:09 +0800

    refactor(types): 更新环境变量类型定义并配置 tsconfig
    - 重命名文件为 env.d.ts，专门用于环境变量的类型定义
    - 添加 NodeJS.ProcessEnv 接口，定义 NODE_ENV 的可能值
    - 在 tsconfig.json 中添加路径映射，使用 @ 作为 src 目录的别名

--- templates/react/src/dts/env.d.ts
+++ templates/react/src/dts/env.d.ts
@@ -1,6 +1,14 @@
 /**
- * @file types.d.ts
+ * @file env.d.ts
+ * @ref https://nodejs.org/api/process.html#processenv
  */
 
 /// <reference types="vite/client" />
 /// <reference types="vitest/globals" />
+
+
+namespace NodeJS {
+  interface ProcessEnv {
+    readonly NODE_ENV: 'development' | 'production' | 'test';
+  }
+}
