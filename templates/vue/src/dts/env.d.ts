02d14e5193d01fbd73cb1378552d7d4e9d2a6bf9
☁️淡然 <hi@ydr.me>
2025-01-24 14:25:26 +0800

    refactor(types): 优化 TypeScript 类型定义和配置
    - 修改 env.d.ts 文件，增加 NodeJS.ProcessEnv 接口定义
    - 更新 tsconfig.json，添加路径映射配置
    - 优化文件注释和引用结构

--- templates/vue/src/dts/env.d.ts
+++ templates/vue/src/dts/env.d.ts
@@ -1,7 +1,15 @@
 /**
- * @file types.d.ts
+ * @file env.d.ts
+ * @ref https://nodejs.org/api/process.html#processenv
  */
 
 /// <reference types="vite/client" />
 /// <reference types="vitest/globals" />
 /// <reference types="unplugin-vue-router/client" />
+
+namespace NodeJS {
+  interface ProcessEnv {
+    readonly NODE_ENV: 'development' | 'production' | 'test';
+  }
+}
+
